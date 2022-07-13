b1ae6d98849a3298f38f23110282fb7e13bf7bc7
Jdtonde <ton2aniel@gmail.com>
2022-07-13 23:44:59 +0000

    end of basics project

--- src/app/recipes/recipes-detail/recipes-detail.component.ts
+++ src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-recipes-detail',
   templateUrl: './recipes-detail.component.html',
   styleUrls: ['./recipes-detail.component.css']
 })
 export class RecipesDetailComponent implements OnInit {
-
+   faCaretDown=faCaretDown;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipes } from './recipes.model';
 
 @Component({
   selector: 'app-recipes',
@@ -7,9 +6,7 @@ import { Recipes } from './recipes.model';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-  recipe: Recipes[]= [
-    new Recipes("Steak","this is a test but meat",30,"https://c.pxhere.com/photos/c6/93/Beef_Foods_Meat_Meats_Steak_Steaks-1621668.jpg!d")
-  ];
+  
   constructor() { }
 
   ngOnInit(): void {
