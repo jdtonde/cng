b53f3292df0b1c10396bebbc4a6f5d02b9eb59ed
Jdtonde <ton2aniel@gmail.com>
2022-07-14 01:19:20 +0000

    basics finshed

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,15 +7,11 @@ import { Ingredients } from '../shared/ingredients.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-<<<<<<< HEAD
  ingredient:Ingredients[]=[
   new Ingredients("Oignons",5),
   new Ingredients("Tomatoes",7),
   
  ];
-=======
- ingredients=[];
->>>>>>> b1ae6d98849a3298f38f23110282fb7e13bf7bc7
   constructor() { }
 
   ngOnInit(): void {
